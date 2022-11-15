854bbc0831071d48f6b4ed3b0c8773591d58f1ee
Yasin <Yasin@192.168.12.95>
2022-11-15 11:55:17 +0100

    Añadimos los get y set

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -5,4 +5,30 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	public Posicion(int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
+	}
+	
+	public Posicion(Posicion posicion) {
+		if (posicion==null) {
+			throw new NullPointerException("la posicion no puede ser nula");
+		}
+	}
+
+	public int getFila() {
+		return fila;
+	}
+
+	public void setFila(int fila) {
+		this.fila = fila;
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	public void setColumna(char columna) {
+		this.columna = columna;
+	}
 }
