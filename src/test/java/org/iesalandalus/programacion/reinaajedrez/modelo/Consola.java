ec5160ab87b7fdeb98299575e4dd84b212399b4b
Yasin <Yasin@192.168.12.95>
2022-11-18 12:23:34 +0100

    Creamos el menu

---
+++ src/test/java/org/iesalandalus/programacion/reinaajedrez/modelo/Consola.java
@@ -0,0 +1,16 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public class Consola {
+	
+	private  Consola() {
+		
+	}
+	
+	public void mostrarMenu () {
+		System.out.println("1/ Crear reina por defecto");
+		System.out.println("2/ Crear reina con color");
+		System.out.println("3/ Mover");
+		System.out.println("4/ Salir");
+	}
+	
+}
